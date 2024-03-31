e18d841cad621a0073a5f68fa143040b19cc36ac
daviddexteel <dsilvestri@dexteel.com>
2024-03-31 19:08:29 -0300

    13 - Ejercicio 01

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
+	"github.com/ddessilvestri/godesde0/ejercicios"
 	"github.com/ddessilvestri/godesde0/variables"
 )
 
 func main() {
+	num, texto := ejercicios.ConvNumerico("x")
+
+	fmt.Println(num)
+	fmt.Println(texto)
+
 	estado, texto := variables.ConvertoaText(1858)
 	fmt.Println(estado)
 	fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es windows")
+	} else {
+		fmt.Println("Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Linux")
+	case "darwin":
+		fmt.Println("Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
