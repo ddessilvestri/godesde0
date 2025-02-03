2cd4fe20bc10ec7c2a0fc4913d403ee633eae0ca
daviddexteel <dsilvestri@dexteel.com>
2025-02-03 19:23:06 -0300

    if and switch

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/ddessilvestri/godesde0/variables"
 )
@@ -12,4 +13,22 @@ func main() {
 	state, text := variables.ConvertToText(1588)
 	fmt.Println("state", state)
 	fmt.Println("text", text)
+
+	// os:= runtime.GOOS
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Linux or OS", os)
+	} else {
+		fmt.Println("should be windows: ", os)
+
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("linux")
+	case "darwin":
+		fmt.Println("darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
